e0873ba566a9874cfcfb54fe579ac669bfc03ec5
adu-steve <adustephen255@gmail.com>
2023-05-24 23:00:55 +0000

    Various functions set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { ExpenseItem } from "./components/ExpenseItem";
+import { Alert } from "./components/Alert";
+import { ExpenseForm } from "./components/ExpenseForm";
 
 function App() {
-  return <div>Hello World.</div>;
+  return (
+    <>
+      <Alert />
+      <ExpenseItem />
+    </>
+  );
 }
 
 export default App;
