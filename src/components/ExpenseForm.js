3fa14d8f0e0e9980c342887cdb412e979f454e31
adu-steve <adustephen255@gmail.com>
2023-05-31 00:10:58 +0000

    Budget functionality
    New budget can be typed and added to the list of budgets already available using the submit button

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { MdSend } from "react-icons/md";
 
-export const ExpenseForm = () => {
-  return <div>ExpenseForm</div>;
+export const ExpenseForm = ({ ...props }) => {
+  const { charge, amount, handleAmount, handleSubmit, handleCharge } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">Charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+            value={charge}
+            onChange={handleCharge}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Charge</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 1000"
+            value={amount}
+            onChange={handleAmount}
+          ></input>
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        Submit
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
 };
